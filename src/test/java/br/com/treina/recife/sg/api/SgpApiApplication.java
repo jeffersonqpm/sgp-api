25bab3c323812cf5a24df6000859d003723d1e48
jeffersonqpm <jefferson.qpm@gmail.com>
2026-04-26 13:49:29 -0300

    atualizando o SgpApiApplication

---
+++ src/test/java/br/com/treina/recife/sg/api/SgpApiApplication.java
@@ -0,0 +1,28 @@
+package br.com.treina.recife.sg.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgpApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
+
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @SpringBootApplication
+// public class SgpApiApplication {
+
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(SgpApiApplication.class, args);
+
+// 		System.out.println(">>> Application loaded successfully! <<<");
+// 	}
+
+// }
